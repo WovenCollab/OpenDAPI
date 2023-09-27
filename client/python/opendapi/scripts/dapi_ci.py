cb53f131a278bbf533d97238baf383adca8c96b1
Karthik Ravichandran <karthik@wovencollab.com>
2023-09-27 02:18:59 -0700

    Github CI Action to interact with a DAPI server

---
+++ client/python/opendapi/scripts/dapi_ci.py
@@ -0,0 +1,341 @@
+"""Python script to Validate, Register and analyze impact of DAPIs with a DAPI Server."""
+
+import json
+import os
+import subprocess  # nosec: B404
+import sys
+
+from dataclasses import dataclass
+from urllib.parse import urljoin
+
+import requests
+
+from opendapi.validators.teams import TeamsValidator
+from opendapi.validators.dapi import DapiValidator
+from opendapi.validators.datastores import DatastoresValidator
+from opendapi.validators.purposes import PurposesValidator
+
+
+@dataclass
+class ChangeTriggerEvent:
+    """Change trigger event, e.g. from Github Actions"""
+
+    event_type: str
+    before_change_sha: str
+    after_change_sha: str
+    git_ref: str | None = None
+
+
+@dataclass
+class DAPIServerConfig:
+    """Configuration for the DAPI Server."""
+
+    server_host: str
+    api_key: str
+    mainline_branch_name: str
+    register_on_merge_to_mainline: bool
+    suggest_changes: bool = True
+
+
+@dataclass
+class DAPIServerResponse:
+    """DAPI server Response formatted"""
+
+    status_code: int
+    error: str | None = None
+    json: dict | None = None
+    text: str | None = None
+    markdown: str | None = None
+
+
+@dataclass
+class OpenDAPIFileContents:
+    """Set of OpenDAPI files."""
+
+    teams: dict[str, dict]
+    dapis: dict[str, dict]
+    datastores: dict[str, dict]
+    purposes: dict[str, dict]
+    root_dir: str
+
+    def contents_as_dict(self):
+        """Convert to a dictionary."""
+        return {
+            "teams": self.teams,
+            "dapis": self.dapis,
+            "datastores": self.datastores,
+            "purposes": self.purposes,
+        }
+
+    @staticmethod
+    def _prune_root_dir(location: str, root_dir: str):
+        """Prune the root dir from the location."""
+        return location[len(root_dir) + 1 :]
+
+    def for_server(self):
+        """Convert to a format ready for the DAPI Server."""
+        result = {}
+        for result_key, contents in self.contents_as_dict().items():
+            result[result_key] = {
+                self._prune_root_dir(location, self.root_dir): json_content
+                for location, json_content in contents.items()
+            }
+        return result
+
+
+class DAPIServerAdapter:
+    """Adapter to interact with the DAPI Server."""
+
+    def __init__(
+        self,
+        repo_root_dir: str,
+        dapi_server_config: DAPIServerConfig,
+        trigger_event: ChangeTriggerEvent,
+    ) -> None:
+        """Initialize the adapter."""
+        self.dapi_server_config = dapi_server_config
+        self.trigger_event = trigger_event
+        self.repo_root_dir = repo_root_dir
+
+        self.all_files: OpenDAPIFileContents = self.get_all_opendapi_files()
+        self.changed_files: OpenDAPIFileContents = self.get_changed_opendapi_files(
+            self.trigger_event.before_change_sha, self.trigger_event.after_change_sha
+        )
+
+    @staticmethod
+    def display_markdown_summary(message: str):
+        """Set the message to be displayed on the DAPI Server."""
+        if "GITHUB_STEP_SUMMARY" in os.environ:
+            with open(
+                os.environ["GITHUB_STEP_SUMMARY"], "a", encoding="utf-8"
+            ) as file_ptr:
+                print(f"{message}\n\n", file=file_ptr)
+        print(message)
+
+    def get_all_opendapi_files(self) -> OpenDAPIFileContents:
+        """Get files from the DAPI Server."""
+
+        result: dict[str, dict[str, dict]] = {}
+        for result_key, validator_cls in {
+            "teams": TeamsValidator,
+            "dapis": DapiValidator,
+            "datastores": DatastoresValidator,
+            "purposes": PurposesValidator,
+        }.items():
+            result[result_key] = validator_cls(
+                self.repo_root_dir,
+                enforce_existence=False,
+                should_autoupdate=False,
+            ).parsed_files
+        return OpenDAPIFileContents(**result, root_dir=self.repo_root_dir)
+
+    @staticmethod
+    def git_diff_filenames(before_change_sha: str, after_change_sha: str) -> list[str]:
+        """Get the list of files changed between two commits."""
+        try:
+            files = subprocess.check_output(
+                ["git", "diff", "--name-only", before_change_sha, after_change_sha]
+            )  # nosec
+            return [
+                filename for filename in files.decode("utf-8").split("\n") if filename
+            ]
+        except subprocess.CalledProcessError as exc:
+            print(f"Error while getting git diff: {exc}", file=sys.stderr)
+            sys.exit(1)
+
+    def get_changed_opendapi_files(
+        self, before_change_sha: str, after_change_sha: str
+    ) -> OpenDAPIFileContents:
+        """Get files changed between two commits."""
+
+        changed_files = self.git_diff_filenames(before_change_sha, after_change_sha)
+
+        result: dict[str, dict[str, dict]] = {}
+
+        for result_key, files in self.all_files.contents_as_dict().items():
+            result[result_key] = {}
+            for filename, file_contents in files.items():
+                for changed_file in changed_files:
+                    if filename.endswith(changed_file):
+                        result[result_key][filename] = file_contents
+        return OpenDAPIFileContents(**result, root_dir=self.repo_root_dir)
+
+    def ask_dapi_server(self, request_path: str, payload: dict) -> DAPIServerResponse:
+        """Ask the DAPI Server for something."""
+        headers = {
+            "Content-Type": "application/json",
+            "X-DAPI-Server-API-Key": self.dapi_server_config.api_key,
+        }
+
+        response = requests.post(
+            urljoin(self.dapi_server_config.server_host, request_path),
+            headers=headers,
+            json=payload,
+            timeout=30,
+        )
+
+        # Server responds with a detailed error on 400, so only error when status > 400
+        if response.status_code > 400:
+            self.display_markdown_summary(
+                f"Something went wrong! API failure with {response.status_code} for {request_path}"
+            )
+            sys.exit(1)
+
+        message = response.json()
+
+        if "error" in message.get("json") and message["json"]["error"]:
+            self.display_markdown_summary(message["text"])
+
+        return DAPIServerResponse(
+            status_code=response.status_code,
+            error=message.get("error"),
+            markdown=message.get("md"),
+            text=message.get("text"),
+            json=message.get("json"),
+        )
+
+    def summarize_output(self, resp: DAPIServerResponse):
+        """Summarize the output of a request to the DAPI Server."""
+        if resp.markdown:
+            self.display_markdown_summary(resp.markdown)
+
+        if resp.json:
+            self.display_markdown_summary(
+                f"```json\n{json.dumps(resp.json, indent=2)}\n```"
+            )
+
+    def validate(self) -> DAPIServerResponse:
+        """Validate OpenDAPI files with the DAPI Server."""
+        all_files = self.all_files.for_server()
+        return self.ask_dapi_server(
+            "/v1/registry/validate",
+            {
+                "dapis": all_files["dapis"],
+                "teams": all_files["teams"],
+                "datastores": all_files["datastores"],
+                "purposes": all_files["purposes"],
+                "suggest_changes": self.dapi_server_config.suggest_changes,
+            },
+        )
+
+    def should_register(self) -> bool:
+        """Check if we should register with the DAPI Server."""
+        if (
+            self.dapi_server_config.register_on_merge_to_mainline
+            and self.trigger_event.event_type == "push"
+            and self.trigger_event.git_ref
+            == f"refs/heads/{self.dapi_server_config.mainline_branch_name}"
+        ):
+            return True
+        self.display_markdown_summary(
+            "Registration skipped because the conditions weren't met"
+        )
+        return False
+
+    def register(self) -> DAPIServerResponse:
+        """Register OpenDAPI files with the DAPI Server."""
+        if not self.should_register():
+            return DAPIServerResponse(status_code=200)
+
+        all_files = self.all_files.for_server()
+        return self.ask_dapi_server(
+            "/v1/registry/register",
+            {
+                "dapis": all_files["dapis"],
+                "teams": all_files["teams"],
+                "datastores": all_files["datastores"],
+                "purposes": all_files["purposes"],
+                "commit_hash": self.trigger_event.after_change_sha,
+            },
+        )
+
+    def analyze_impact(self) -> DAPIServerResponse:
+        """Analyze the impact of changes on the DAPI Server."""
+        changed_files = self.changed_files.for_server()
+        return self.ask_dapi_server(
+            "/v1/registry/impact",
+            {
+                "dapis": changed_files["dapis"],
+                "teams": changed_files["teams"],
+                "datastores": changed_files["datastores"],
+                "purposes": changed_files["purposes"],
+            },
+        )
+
+    def retrieve_stats(self):
+        """Retrieve stats from the DAPI Server."""
+        changed_files = self.changed_files.for_server()
+        return self.ask_dapi_server(
+            "/v1/registry/stats",
+            {
+                "dapis": changed_files["dapis"],
+                "teams": changed_files["teams"],
+                "datastores": changed_files["datastores"],
+                "purposes": changed_files["purposes"],
+            },
+        )
+
+    def run(self):
+        """Run the action."""
+
+        self.display_markdown_summary("## Step 1: Validating OpenDAPI files...")
+        self.summarize_output(self.validate())
+
+        self.display_markdown_summary("## Step 2: Registering...")
+        self.summarize_output(self.register())
+
+        self.display_markdown_summary("## Step 3: Analyzing impact of changes...")
+        self.summarize_output(self.analyze_impact())
+
+        self.display_markdown_summary("## Step 4: Retrieving stats...")
+        self.summarize_output(self.retrieve_stats())
+
+
+def main():
+    """Run the action."""
+    DAPIServerAdapter.display_markdown_summary("# OpenDAPI CI")
+    DAPIServerAdapter.display_markdown_summary(
+        "Here we will validate, register, and analyze the impact of changes to OpenDAPI files."
+    )
+
+    dapi_server_config = DAPIServerConfig(
+        server_host=os.environ["DAPI_SERVER_HOST"],
+        api_key=os.environ["DAPI_SERVER_API_KEY"],
+        mainline_branch_name=os.environ["MAINLINE_BRANCH_NAME"],
+        register_on_merge_to_mainline=os.environ["REGISTER_ON_MERGE_TO_MAINLINE"],
+        suggest_changes=os.environ["SUGGEST_CHANGES"],
+    )
+
+    # Rebuild github context from environment variables
+    gh_context: dict = {
+        "event_name": os.environ["GITHUB_EVENT_NAME"],
+        "root_dir": os.environ["GITHUB_WORKSPACE"],
+    }
+    with open(os.environ["GITHUB_EVENT_PATH"], "r", encoding="utf-8") as file_ptr:
+        gh_context["event"] = json.load(file_ptr)
+
+    if gh_context["event_name"] not in ["push", "pull_request"]:
+        DAPIServerAdapter.display_markdown_summary(
+            f"Uh ho! Error! Unsupported event type: {gh_context['event_name']}"
+        )
+        sys.exit(1)
+
+    change_trigger_event = ChangeTriggerEvent(
+        event_type=gh_context["event_name"],
+        before_change_sha=gh_context["event"]["before"]
+        if gh_context["event_name"] == "push"
+        else gh_context["event"]["pull_request"]["base"]["sha"],
+        after_change_sha=gh_context["event"]["after"]
+        if gh_context["event_name"] == "push"
+        else gh_context["event"]["pull_request"]["head"]["sha"],
+        git_ref=gh_context["event"]["ref"]
+        if gh_context["event_name"] == "push"
+        else None,
+    )
+
+    dapi_server_adapter = DAPIServerAdapter(
+        repo_root_dir=gh_context["root_dir"],
+        dapi_server_config=dapi_server_config,
+        trigger_event=change_trigger_event,
+    )
+    dapi_server_adapter.run()
---
+++ client/python/tests/scripts/test_dapi_ci.py
@@ -0,0 +1,445 @@
+# pylint: disable=unused-argument
+"""Tests script/dapi_ci.py"""
+import os
+import subprocess
+from unittest import mock
+
+import pytest
+
+from opendapi.scripts.dapi_ci import (
+    ChangeTriggerEvent,
+    DAPIServerConfig,
+    DAPIServerAdapter,
+    OpenDAPIFileContents,
+    main,
+)
+from opendapi.validators.dapi import DapiValidator
+from opendapi.validators.datastores import DatastoresValidator
+from opendapi.validators.purposes import PurposesValidator
+from opendapi.validators.teams import TeamsValidator
+
+
+@pytest.fixture(name="sample_dapi_ci_server_config")
+def fixture_sample_dapi_ci_server_config():
+    """Return a sample DAPI server config"""
+    return DAPIServerConfig(
+        server_host="https://example.com",
+        api_key="your_api_key",
+        mainline_branch_name="main",
+        register_on_merge_to_mainline=True,
+        suggest_changes=True,
+    )
+
+
+@pytest.fixture(name="sample_dapi_ci_trigger_push")
+def fixture_sample_dapi_ci_trigger_push():
+    """Return a sample DAPI CI trigger event"""
+    return ChangeTriggerEvent(
+        event_type="push",
+        before_change_sha="before_sha",
+        after_change_sha="after_sha",
+        git_ref="refs/heads/main",
+    )
+
+
+@pytest.fixture(name="sample_dapi_ci_trigger_pull_request")
+def fixture_sample_dapi_ci_trigger_pull_request():
+    """Return a sample DAPI CI trigger event"""
+    return ChangeTriggerEvent(
+        event_type="pull_request",
+        before_change_sha="before_sha",
+        after_change_sha="after_sha",
+        git_ref="refs/pull/1/merge",
+    )
+
+
+@pytest.fixture(name="sample_opendapi_file_contents")
+def fixture_sample_opendapi_file_contents(
+    mocker, valid_teams, valid_dapi, valid_datastores, valid_purposes
+):
+    """Return a sample OpenDAPI file contents"""
+    m_teams_files = {
+        "/path/to/repo/1.teams.yaml": valid_teams,
+        "/path/to/repo/2.teams.yaml": valid_teams,
+    }
+    mocker.patch.object(
+        TeamsValidator, "_get_file_contents_for_suffix", return_value=m_teams_files
+    )
+    m_dapis_files = {
+        "/path/to/repo/1.dapi.yaml": valid_dapi,
+        "/path/to/repo/2.dapi.yaml": valid_dapi,
+    }
+    mocker.patch.object(
+        DapiValidator, "_get_file_contents_for_suffix", return_value=m_dapis_files
+    )
+    m_datastores_files = {
+        "/path/to/repo/1.datastores.yaml": valid_datastores,
+        "/path/to/repo/2.datastores.yaml": valid_datastores,
+    }
+    mocker.patch.object(
+        DatastoresValidator,
+        "_get_file_contents_for_suffix",
+        return_value=m_datastores_files,
+    )
+    m_purposes_files = {
+        "/path/to/repo/1.purposes.yaml": valid_purposes,
+        "/path/to/repo/2.purposes.yaml": valid_purposes,
+    }
+    mocker.patch.object(
+        PurposesValidator,
+        "_get_file_contents_for_suffix",
+        return_value=m_purposes_files,
+    )
+
+    return OpenDAPIFileContents(
+        teams=m_teams_files,
+        dapis=m_dapis_files,
+        datastores=m_datastores_files,
+        purposes=m_purposes_files,
+        root_dir="/path/to/repo",
+    )
+
+
+@pytest.fixture(autouse=True)
+def setup(mocker, temp_directory):
+    """Mock some things"""
+    mocker.patch.dict(
+        os.environ,
+        {
+            "DAPI_SERVER_HOST": "https://example.com",
+            "DAPI_SERVER_API_KEY": "your-api-key",
+            "MAINLINE_BRANCH_NAME": "main",
+            "REGISTER_ON_MERGE_TO_MAINLINE": "True",
+            "SUGGEST_CHANGES": "True",
+            "GITHUB_EVENT_NAME": "push",
+            "GITHUB_WORKSPACE": "/path/to/repo",
+            "GITHUB_EVENT_PATH": f"{temp_directory}/trigger_event.json",
+            "GITHUB_STEP_SUMMARY": f"{temp_directory}/output.txt",
+        },
+    )
+    mocker.patch(
+        "json.load",
+        return_value={
+            "event_name": "push",
+            "before": "before_sha",
+            "after": "after_sha",
+            "ref": "refs/heads/main",
+        },
+    )
+    mocker.patch(
+        "subprocess.check_output",
+        return_value=(
+            b"2.dapi.yaml\n2.teams.yaml\n" b"2.datastores.yaml\n2.purposes.yaml\n"
+        ),
+    )
+    yield
+
+
+def test_dapi_server_adapter_init(
+    sample_dapi_ci_server_config, sample_dapi_ci_trigger_push
+):
+    """Test DAPIServerAdapter init"""
+    adapter = DAPIServerAdapter(
+        repo_root_dir="/path/to/repo",
+        dapi_server_config=sample_dapi_ci_server_config,
+        trigger_event=sample_dapi_ci_trigger_push,
+    )
+    assert adapter.dapi_server_config.server_host == "https://example.com"
+    assert adapter.trigger_event.git_ref == "refs/heads/main"
+    assert adapter.repo_root_dir == "/path/to/repo"
+
+
+def test_dapi_server_adapter_should_register(
+    sample_dapi_ci_server_config,
+    sample_dapi_ci_trigger_push,
+    sample_dapi_ci_trigger_pull_request,
+):
+    """Test DAPIServerAdapter.should_register"""
+    adapter = DAPIServerAdapter(
+        repo_root_dir="/path/to/repo",
+        dapi_server_config=sample_dapi_ci_server_config,
+        trigger_event=sample_dapi_ci_trigger_push,
+    )
+    assert adapter.should_register() is True
+
+    adapter = DAPIServerAdapter(
+        repo_root_dir="/path/to/repo",
+        dapi_server_config=sample_dapi_ci_server_config,
+        trigger_event=sample_dapi_ci_trigger_pull_request,
+    )
+    assert adapter.should_register() is False
+
+
+def test_dapi_server_adapter_git_diff_filenames(mocker):
+    """Test DAPIServerAdapter.git_diff_filenames"""
+    filenames = DAPIServerAdapter.git_diff_filenames("before_sha", "after_sha")
+    assert filenames == [
+        "2.dapi.yaml",
+        "2.teams.yaml",
+        "2.datastores.yaml",
+        "2.purposes.yaml",
+    ]
+
+    # mock subprocess.check_output error
+    mocker.patch(
+        "subprocess.check_output",
+        side_effect=subprocess.CalledProcessError(0, "Something went wrong"),
+    )
+    with pytest.raises(SystemExit):
+        DAPIServerAdapter.git_diff_filenames("before_sha", "after_sha")
+
+
+def test_dapi_server_adapter_get_changed_opendapi_files(
+    sample_opendapi_file_contents,
+    sample_dapi_ci_server_config,
+    sample_dapi_ci_trigger_push,
+):
+    """Test DAPIServerAdapter.get_changed_opendapi_files"""
+    adapter = DAPIServerAdapter(
+        repo_root_dir="/path/to/repo",
+        dapi_server_config=sample_dapi_ci_server_config,
+        trigger_event=sample_dapi_ci_trigger_push,
+    )
+
+    changed_files = adapter.get_changed_opendapi_files(
+        before_change_sha="before_sha", after_change_sha="after_sha"
+    )
+
+    assert isinstance(changed_files, OpenDAPIFileContents)
+    assert "/path/to/repo/2.dapi.yaml" in changed_files.dapis
+    assert "/path/to/repo/2.teams.yaml" in changed_files.teams
+    assert "/path/to/repo/2.datastores.yaml" in changed_files.datastores
+    assert "/path/to/repo/2.purposes.yaml" in changed_files.purposes
+
+
+def test_dapi_server_adapter_validate(
+    sample_opendapi_file_contents,
+    sample_dapi_ci_server_config,
+    sample_dapi_ci_trigger_push,
+):
+    """Test DAPIServerAdapter.validate"""
+    adapter = DAPIServerAdapter(
+        repo_root_dir="/path/to/repo",
+        dapi_server_config=sample_dapi_ci_server_config,
+        trigger_event=sample_dapi_ci_trigger_push,
+    )
+
+    with mock.patch("requests.post") as mock_post:
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json.return_value = {
+            "md": "Validation successful",
+            "json": {"success": True},
+        }
+
+        adapter.validate()
+
+        assert mock_post.called
+        _, kwargs = mock_post.call_args
+        expected = {
+            key: sample_opendapi_file_contents.for_server()[key]
+            for key in ["dapis", "teams", "datastores", "purposes"]
+        }
+        assert kwargs["json"] == {"suggest_changes": True, **expected}
+
+
+def test_dapi_server_adapter_validate_fails(
+    sample_opendapi_file_contents,
+    sample_dapi_ci_server_config,
+    sample_dapi_ci_trigger_push,
+):
+    """Test DAPIServerAdapter.validate"""
+    adapter = DAPIServerAdapter(
+        repo_root_dir="/path/to/repo",
+        dapi_server_config=sample_dapi_ci_server_config,
+        trigger_event=sample_dapi_ci_trigger_push,
+    )
+
+    with mock.patch("requests.post") as mock_post:
+        mock_post.return_value.status_code = 500
+
+        with pytest.raises(SystemExit):
+            adapter.validate()
+
+        assert mock_post.called
+
+
+def test_dapi_server_adapter_validate_returns_error_message(
+    sample_opendapi_file_contents,
+    sample_dapi_ci_server_config,
+    sample_dapi_ci_trigger_push,
+):
+    """Test DAPIServerAdapter.validate"""
+    adapter = DAPIServerAdapter(
+        repo_root_dir="/path/to/repo",
+        dapi_server_config=sample_dapi_ci_server_config,
+        trigger_event=sample_dapi_ci_trigger_push,
+    )
+
+    with mock.patch("requests.post") as mock_post:
+        mock_post.return_value.status_code = 400
+        mock_post.return_value.json.return_value = {
+            "md": "Validation failed",
+            "json": {"success": False, "error": True},
+            "text": "Error message",
+        }
+
+        adapter.validate()
+        assert mock_post.called
+
+
+def test_dapi_server_adapter_register(
+    sample_opendapi_file_contents,
+    sample_dapi_ci_server_config,
+    sample_dapi_ci_trigger_push,
+):
+    """Test DAPIServerAdapter.register"""
+
+    adapter = DAPIServerAdapter(
+        repo_root_dir="/path/to/repo",
+        dapi_server_config=sample_dapi_ci_server_config,
+        trigger_event=sample_dapi_ci_trigger_push,
+    )
+
+    with mock.patch("requests.post") as mock_post:
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json.return_value = {
+            "md": "Registration successful",
+            "json": {"success": True},
+        }
+
+        adapter.register()
+
+        assert mock_post.called
+        _, kwargs = mock_post.call_args
+        expected = {
+            key: sample_opendapi_file_contents.for_server()[key]
+            for key in ["dapis", "teams", "datastores", "purposes"]
+        }
+        assert kwargs["json"] == {"commit_hash": "after_sha", **expected}
+
+
+def test_dapi_server_adapter_register_only_when_appropriate(
+    sample_opendapi_file_contents,
+    sample_dapi_ci_server_config,
+    sample_dapi_ci_trigger_pull_request,
+):
+    """Test DAPIServerAdapter.register"""
+
+    adapter = DAPIServerAdapter(
+        repo_root_dir="/path/to/repo",
+        dapi_server_config=sample_dapi_ci_server_config,
+        trigger_event=sample_dapi_ci_trigger_pull_request,
+    )
+
+    with mock.patch("requests.post") as mock_post:
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json.return_value = {
+            "md": "Registration successful",
+            "json": {"success": True},
+        }
+
+        adapter.register()
+        mock_post.assert_not_called()
+
+
+def test_dapi_server_adapter_analyze_impact(
+    sample_opendapi_file_contents,
+    sample_dapi_ci_server_config,
+    sample_dapi_ci_trigger_push,
+):
+    """Test DAPIServerAdapter.analyze_impact"""
+    adapter = DAPIServerAdapter(
+        repo_root_dir="/path/to/repo",
+        dapi_server_config=sample_dapi_ci_server_config,
+        trigger_event=sample_dapi_ci_trigger_push,
+    )
+
+    with mock.patch("requests.post") as mock_post:
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json.return_value = {
+            "md": "Impact analysis successful",
+            "json": {"success": True},
+        }
+
+        adapter.analyze_impact()
+
+        assert mock_post.called
+        _, kwargs = mock_post.call_args
+        assert kwargs["json"] == {
+            key: adapter.changed_files.for_server()[key]
+            for key in ["dapis", "teams", "datastores", "purposes"]
+        }
+
+
+def test_dapi_server_adapter_retrieve_stats(
+    sample_opendapi_file_contents,
+    sample_dapi_ci_server_config,
+    sample_dapi_ci_trigger_push,
+):
+    """Test DAPIServerAdapter.retrieve_stats"""
+    adapter = DAPIServerAdapter(
+        repo_root_dir="/path/to/repo",
+        dapi_server_config=sample_dapi_ci_server_config,
+        trigger_event=sample_dapi_ci_trigger_push,
+    )
+
+    with mock.patch("requests.post") as mock_post:
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json.return_value = {
+            "md": "Stats retrieved successfully",
+            "json": {"success": True},
+        }
+
+        adapter.retrieve_stats()
+
+        assert mock_post.called
+        _, kwargs = mock_post.call_args
+        assert kwargs["json"] == {
+            key: adapter.changed_files.for_server()[key]
+            for key in ["dapis", "teams", "datastores", "purposes"]
+        }
+
+
+def test_dapi_server_adapter_run(
+    sample_opendapi_file_contents,
+    sample_dapi_ci_server_config,
+    sample_dapi_ci_trigger_push,
+):
+    """Test DAPIServerAdapter.run"""
+    adapter = DAPIServerAdapter(
+        repo_root_dir="/path/to/repo",
+        dapi_server_config=sample_dapi_ci_server_config,
+        trigger_event=sample_dapi_ci_trigger_push,
+    )
+
+    with mock.patch("requests.post") as mock_post:
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json.return_value = {
+            "md": "Validation successful",
+            "json": {"success": True},
+        }
+
+        adapter.run()
+        assert mock_post.call_count == 4
+
+
+def test_main(mocker):
+    """Test the main function"""
+    m_adapter_run = mocker.patch.object(DAPIServerAdapter, "run")
+    mock_open = mocker.mock_open(read_data="dummy")
+    mocker.patch("builtins.open", mock_open)
+    main()
+    m_adapter_run.assert_called_once()
+
+
+def test_main_unsupported_event_name(mocker):
+    """Test the main function"""
+    new_environ = os.environ.copy()
+    new_environ["GITHUB_EVENT_NAME"] = "unsupported"
+    mocker.patch.dict(os.environ, new_environ)
+    m_adapter_run = mocker.patch.object(DAPIServerAdapter, "run")
+    mock_open = mocker.mock_open(read_data="dummy")
+    mocker.patch("builtins.open", mock_open)
+    with pytest.raises(SystemExit):
+        main()
+    m_adapter_run.assert_not_called()
