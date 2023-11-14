d6f730c146d6c8e7c30a523358198b79effe3bca
Karthik Ravichandran <kbravi@users.noreply.github.com>
2023-11-13 18:03:20 -0800

    [CI Action] Comment and PR description copy improvements (#18)
    * Copy improvements for DAPI CI action
    
    * Improvements to Suggestions PR

--- client/python/opendapi/scripts/dapi_ci.py
+++ client/python/opendapi/scripts/dapi_ci.py
@@ -451,8 +451,8 @@ class DAPIServerAdapter:
 
         body += (
             f"We identified data model changes in #{self.trigger_event.pull_request_number} "
-            "and generated updated data documentation for you\n\n. "
-            "Please review and merge if this looks good.\n\n"
+            "and generated updated data documentation for you.\n\n "
+            "Please review and merge into your working branch if this looks good.\n\n"
         )
 
         suggestions_pr_number = self.create_or_update_pull_request(
@@ -628,28 +628,28 @@ class DAPIServerAdapter:
             if validate_resp.server_meta.logo_url:
                 pr_comment_md += (
                     f'<img src="{validate_resp.server_meta.logo_url}"'
-                    ' width="30" valign="middle"/>'
+                    ' width="30" valign="middle"/>  '
                 )
             pr_comment_md += (
-                f"{validate_resp.server_meta.name} Data Documentation AI</a>\n"
+                f"{validate_resp.server_meta.name} Data Documentation AI</a>\n\n"
             )
 
             # Suggestions
             if suggestions_pr_number:
                 pr_comment_md += (
-                    "### :heart: Great looking PR! Review your data model changes\n"
+                    "### :heart: Great looking PR! Review your data model changes\n\n"
                 )
                 pr_comment_md += (
                     "We noticed some data model changes and "
-                    "generated updated data documentation for you."
-                    "We have some suggestions for you.\n"
-                    f"Please review #{suggestions_pr_number} and merge into this Pull Request."
+                    "generated updated data documentation for you. "
+                    "We have some suggestions for you. "
+                    f"Please review #{suggestions_pr_number} and merge into this Pull Request.\n\n"
                 )
                 pr_comment_md += (
                     f'<a href="{self.trigger_event.repo_html_url}/pull/{suggestions_pr_number}">'
                     f'<img src="{validate_resp.server_meta.suggestions_cta_url}" width="140"/>'
                     "</a>"
-                    "\n<hr>\n"
+                    "\n\n<hr>\n\n"
                 )
 
             # Validation Response
